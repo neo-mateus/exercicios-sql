ee947dec4a59b45b3298a7cf00157dd9b5e9f36e
neo-mateus <neomateus33@gmail.com>
2026-07-13 13:52:42 -0300

    Resolução da alternativa A

---
+++ Introdução ao SQL/Exercício 12/resposta12a.sql
@@ -0,0 +1,6 @@
+/*Seguindo a estrutura do esquema montado consideraremos
+CS-001 como o curso 001,e 1 de crédito para atender as 
+restrições de integridade pré-estabelecidas.*/
+
+insert into course
+values (001, "Weekly Seminar", "Comp. Sci.", 1);
