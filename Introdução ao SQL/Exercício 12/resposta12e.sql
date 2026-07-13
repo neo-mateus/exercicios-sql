9962a0356e2ccd592938d55c3a119db6d31ce169
neo-mateus <neomateus33@gmail.com>
2026-07-13 14:21:57 -0300

    Resolução da alternativa E

---
+++ Introdução ao SQL/Exercício 12/resposta12e.sql
@@ -0,0 +1,6 @@
+delete from course
+where course_id = 1;
+
+/*Executar esse comando excluirá por conseguinte todas as 
+seções e ofertas atrelados a este curso, mas por padrão
+a restrição de integridade referencial impediria o comando*/
