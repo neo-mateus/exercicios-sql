51fc977f1fec780654ad7715bdc2c1f857d5e163
neo-mateus <neomateus33@gmail.com>
2026-07-16 08:59:15 -0300

    Resolução do exercício 18

---
+++ Introdução ao SQL/Exercício 18/esquema_empresa.sql
@@ -0,0 +1,27 @@
+create table employed (
+	ID int check (ID > 0),
+    employed_name varchar(20) not null,
+    street varchar(50),
+    city varchar(20),
+    primary key (ID)
+);
+
+create table works(
+	ID int,
+    company_name varchar(30),
+    salary numeric(6,2),
+    foreign key (ID) references employed(ID),
+    foreign key (company_name) references company(company_name)
+);
+create table company(
+	company_name varchar(30),
+    city varchar(20),
+    primary key (company_name)
+);
+
+create table manage(
+	ID int,
+    manager_id int check (manager_id > 0),
+    primary key(ID),
+    foreign key (ID) references employed(ID)
+);
