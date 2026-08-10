ca4874b22fb6bc41f720b8dbde096df913f2de76
neo-mateus <neomateus33@gmail.com>
2026-08-10 20:17:05 -0300

    Resolução do exercício 23

--- Introdução ao SQL/Exercício 23/resposta23.sql
+++ Introdução ao SQL/Exercício 23/resposta23.sql
@@ -4,7 +4,7 @@ from (select dept_name, sum(salary) as value
      group by dept_name) as total_dept,
      (select avg(value2) as value1
 		from (select sum(salary) as value2
-		from instructor
-		group by dept_name) as total_dept) 
+			from instructor
+			group by dept_name) as total_dept) 
         as media_dept
 where total_dept.value > media_dept.value1;
